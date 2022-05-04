67478f6972a242d260ce403589c81e006ea4cccf
Maksim <ilyshev@mail.ru>
2022-05-04 18:05:50 +0300

    PageUp icon + Animations
    Add padeup icon, smooth scroll, animation for advantages icon and feedbacks

--- src/js/script.js
+++ src/js/script.js
@@ -120,4 +120,23 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    // smooth scroll & page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#'").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    
+            
 });
